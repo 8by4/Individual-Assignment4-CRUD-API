87144ca045e96b8ec6b6dce840de3a277938faa4
8by4 <aidanquinn147@gmail.com>
2024-10-25 12:29:13 -0400

    format and remove old code

--- src/main/java/aq340/IA4/animal/AnimalController.java
+++ src/main/java/aq340/IA4/animal/AnimalController.java
@@ -8,10 +8,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-/**
- * AnimalController.java.
- * Includes all REST API endpoint mappings for the Animal object.
- */
 @Controller
 @RequestMapping("/animals")
 public class AnimalController {
@@ -19,12 +15,6 @@ public class AnimalController {
     @Autowired
     private AnimalService service;
 
-    /**
-     * Get a list of all Animals in the database.
-     * http://localhost:8080/animals/all
-     *
-     * @return a list of Animal objects.
-     */
     @GetMapping("/all")
     public String getAllAnimals(Model model) {
         model.addAttribute("animalList", service.getAllAnimals());
@@ -33,13 +23,6 @@ public class AnimalController {
         return "animal-list";
     }
 
-    /**
-     * Get a specific Animal by Id.
-     * http://localhost:8080/animals/2
-     *
-     * @param animalId the unique Id for an Animal.
-     * @return One Animal object.
-     */
     @GetMapping("/{animalId}")
     public String getOneAnimal(@PathVariable int animalId, Model model) {
         model.addAttribute("animal", service.getAnimalById(animalId));
@@ -48,13 +31,6 @@ public class AnimalController {
         return "animal-details";
     }
 
-    /**
-     * Get a list of Animals based on their species.
-     * http://localhost:8080/animals?species=dog
-     *
-     * @param species the search key.
-     * @return A list of Animal objects matching the species key.
-     */
     @GetMapping("")
     public String getAnimalsBySpecies(@RequestParam(name = "species", defaultValue = "wolf") String species, Model model) {
         model.addAttribute("animalList", service.getAnimalsBySpecies(species));
@@ -63,14 +39,6 @@ public class AnimalController {
         return "animal-list";
     }
 
-
-    /**
-     * Get a list of Animals based on their name with a string.
-     * http://localhost:8080/animals/search?string=blue
-     *
-     * @param search the search key.
-     * @return A list of Animal objects matching the string search key.
-     */
     @GetMapping("/search")
     public String getAnimalsByString(@RequestParam String string, Model model) {
         model.addAttribute("animalList", service.getAnimalsByString(string));
@@ -79,50 +47,24 @@ public class AnimalController {
         return "animal-details";
     }
 
-
     @GetMapping("/new")
     public String addNewAnimal(@PathVariable int animalId, Model model) {
         model.addAttribute("animal", service.getAnimalById(animalId));
         return "animal-create";
     }
 
-
-    /**
-     * Create a new Animal entry.
-     * http://localhost:8080/animals/new --data '{ "animalId": 4, "name": "sample",
-     *                                    "description": "sample desc"}'
-     *
-     * @param animal the new Animal object.
-     * @return the updated list of Animals.
-     */
     @PostMapping("/new")
     public String addNewAnimal(Animal animal) {
         service.addNewAnimal(animal);
         return "redirect:/animals/all";
     }
 
-    /**
-     * Update an existing Animal object.
-     * http://localhost:8080/animals/update/2 --data '{"animalId": 4, "name": "sampleUpdate",
-     *      *                                    "scientificName": Canis Lupus, "species": "wolf", "habitat": "Most forests and mountaints",
-     *      *                                    "description": "sample desc"}'
-     *
-     * @param animalId the unique Animal Id.
-     * @param animal the new update Animal details.
-     * @return the updated Animal object.
-     */
     @GetMapping("/update/{animalId}")
     public String updateAnimal(@PathVariable int animalId, Model model) {
         model.addAttribute("animal", service.getAnimalById(animalId));
         return "animal-update";
     }
 
-
-    /**
-     * Perform the update.
-     * @param animal
-     * @return
-     */
     @PostMapping("/update")
     public String updateAnimal(Animal animal) {
         System.out.println(animal.toString());
@@ -131,14 +73,6 @@ public class AnimalController {
         return "redirect:/animals/" + animal.getAnimalId();
     }
 
-
-    /**
-     * Delete a Animal object.
-     * http://localhost:8080/animals/delete/2
-     *
-     * @param animalId the unique Animal Id.
-     * @return the updated list of Animals.
-     */
     @GetMapping("/delete/{animalId}")
     public String deleteAnimalById(@PathVariable int animalId) {
         service.deleteAnimalById(animalId);
--- src/main/java/aq340/IA4/animal/AnimalService.java
+++ src/main/java/aq340/IA4/animal/AnimalService.java
@@ -7,52 +7,24 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-/**
- * AnimalService.java
- * Centralizes data access to the Animal Database.
- */
 @Service
 public class AnimalService {
 
     @Autowired
     private AnimalRepository animalRepository;
 
-    /**
-     * Fetch all Animals.
-     *
-     * @return the list of all Animals.
-     */
     public List<Animal> getAllAnimals() {
         return animalRepository.findAll();
     }
 
-    /**
-     * Fetch a unique animal.
-     *
-     * @param animalId the unique Animal id.
-     * @return a unique Animal object.
-     */
     public Animal getAnimalById(int animalId) {
         return animalRepository.findById(animalId).orElse(null);
     }
 
-    /**
-     * Fetch all animals whose species matches the search term.
-     *
-     * @param species the search key.
-     * @return the list of matching Animals.
-     */
     public List<Animal> getAnimalsBySpecies(String species) {
         return animalRepository.getAnimalsBySpecies(species);
     }
 
-
-    /**
-     * Fetch all animals whose species matches the search term.
-     *
-     * @param string is the search key.
-     * @return the list of matching Animals with names containing the string.
-     */
     public List<Animal> getAnimalsByString(String string) {
         return animalRepository.findByNameContaining(string);
     }
